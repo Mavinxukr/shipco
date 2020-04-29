3281dc1cd6c722e80a8a4f3fddbc54d7b86ef26d
SamborVadym <samborvadym@gmail.com>
2020-04-29 14:44:49 +0300

    fixed page CLients

--- components/AdminWrapper/Client/Client.jsx
+++ components/AdminWrapper/Client/Client.jsx
@@ -709,7 +709,7 @@ const Table = ({ columns, data, arrAutoId }) => {
     usePagination,
     useRowSelect,
     (hooks) => {
-      hooks.visibleColumns.push(columns => [
+      hooks.flatColumns.push(columns => [
         {
           id: 'selection',
           Header: ({ getToggleAllRowsSelectedProps }) => (
