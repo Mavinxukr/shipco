3bd5ae5f1bc8b490daa37ea28e45b0cdf769fc36
SamborVadym <samborvadym@gmail.com>
2020-05-21 16:23:07 +0300

    fixed price

--- components/AdminWrapper/AddNewPrice/data.js
+++ components/AdminWrapper/AddNewPrice/data.js
@@ -2,3 +2,23 @@ export const type = [
   { value: 'group', label: 'groups' },
   { value: 'client', label: 'clients' },
 ];
+
+export const columnsPrice = [
+  {
+    Header: 'Table',
+    columns: [
+      {
+        Header: 'Cities',
+        accessor: 'name',
+      },
+      {
+        Header: 'State',
+        accessor: 'state',
+      },
+      {
+        Header: 'Price',
+        accessor: 'price',
+      },
+    ],
+  },
+];
