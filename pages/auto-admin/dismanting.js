9c2ca66a9879d0144a704a7aea2c429ef17261cc
Aleksandr Lukin <aleksandrlukin@MacBook-Pro-Aleksandr.local>
2021-05-10 19:45:29 +0300

    page block; ;

--- pages/auto-admin/dismanting.js
+++ pages/auto-admin/dismanting.js
@@ -1,3 +1,28 @@
 import Dismasting from '../../components/AdminWrapper/Dismasting/Dismasting';
+import { getSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Loader from '../components/Loader/Loader';
 
-export default Dismasting;
+function DismastingPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session || session.user.role !== 'admin') {
+        router.replace('/');
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <Loader></Loader>;
+  }
+
+  return <Dismasting />;
+}
+
+export default DismastingPage;
