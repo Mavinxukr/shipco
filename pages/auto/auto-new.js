231c11d60ebe955682fbaa7cb22ad47e4d8c4d9f
Aleksandr Lukin <aleksandrlukin@MacBook-Pro-Aleksandr.local>
2021-05-11 09:45:20 +0300

    fix bug token2

--- pages/auto/auto-new.js
+++ pages/auto/auto-new.js
@@ -1,3 +1,27 @@
 import AutoNew from '../../components/Wrapper/AutoNew/AutoNew';
+import { getSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Loader from '../components/Loader/Loader';
 
-export default AutoNew;
+function AutoNewPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session || session.user.role !== 'user') {
+        router.push('/');
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <Loader></Loader>;
+  }
+
+  return <AutoNew />;
+}
+
+export default AutoNewPage;
